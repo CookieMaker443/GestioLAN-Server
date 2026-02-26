d97b7c23c9430e25603b002875b2b99a2d479a0f
Marco <intensework443@gmail.com>
2026-02-26 22:36:38 +0100

    chore: added a using ferenze to use [Authorize]

--- src/csharpAPI/Controllers/CategoriesController.cs
+++ src/csharpAPI/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using csharpAPI.Models;
+using Microsoft.AspNetCore.Authorization; // Per l'attributo [Authorize]
 
 namespace csharpAPI.Controllers;
 

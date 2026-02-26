d521f96341bd39879879d414e2ff29a3e2369cee
Marco <intensework443@gmail.com>
2026-02-26 18:23:21 +0100

    Migration: Add IsAdmin column to User model and update migration files

---
+++ src/csharpAPI/Migrations/20260226165656_AddIsAdmin.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace csharpAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsAdmin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdmin",
+                table: "user",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "user");
+        }
+    }
+}
